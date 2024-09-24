9cd3c156210d550c3c4e082328641b3d23187ede
BradyShimanek <144560398+BradyShimanek@users.noreply.github.com>
2024-09-24 09:55:35 -0600

    Added more lecture notes to C++

---
+++ CS2370 C++/2370_labs/lab_827_optional/lab_827_optional/Team.cpp
@@ -0,0 +1,8 @@
+//
+//  Team.cpp
+//  lab_827_optional
+//
+//  Created by Brady Shimanek on 9/24/24.
+//
+
+#include <stdio.h>
---
+++ CS2370 C++/2370_labs/lab_827_optional/lab_827_optional/Team.h
@@ -0,0 +1,12 @@
+//
+//  Team.h
+//  lab_827_optional
+//
+//  Created by Brady Shimanek on 9/24/24.
+//
+
+#ifndef Team_h
+#define Team_h
+
+
+#endif /* Team_h */
