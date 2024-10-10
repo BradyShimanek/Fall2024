d5de8dc10e88ba0133adfde89971867ee742ad14
BradyShimanek <144560398+BradyShimanek@users.noreply.github.com>
2024-10-10 11:18:58 -0600

    Added even more files to courses.

--- CS2370 C++/2370_labs/lab_905_exception_handling/lab_905_exception_handling/main.cpp
+++ CS2370 C++/2370_labs/lab_905_exception_handling/lab_905_exception_handling/main.cpp
@@ -1,14 +1,39 @@
-//
-//  main.cpp
-//  lab_905_exception_handling
-//
-//  Created by Brady Shimanek on 10/7/24.
-//
-
+#include <string>
 #include <iostream>
+#include <ios>  // For ios_base::failure
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    string inputName;
+    int age;
+    
+    // Set exception mask for cin stream
+    cin.exceptions(ios::failbit);
+    
+    cin >> inputName;
+    while (inputName != "-1") {
+        try {
+            // Try to read the age. This may throw an ios_base::failure exception.
+            cin >> age;
+            cout << inputName << " " << (age + 1) << endl;  // Increment the age and print
+        } catch (ios_base::failure& e) {
+            // If an exception is thrown (e.g., a string is entered instead of an int), handle it here.
+            
+            // Clear failbit to reset cin so it can be used again
+            cin.clear();
+            
+            // Throw away the rest of the failed input line from the cin buffer
+            string garbage;
+            getline(cin, garbage);
+            
+            // Output the name with age as 0 (since the input was invalid)
+            cout << inputName << " 0" << endl;
+        }
+
+        // Read the next name
+        cin >> inputName;
+    }
+
     return 0;
 }
---
+++ CS2550-WebDev/zybooks_work/helper/script.js
@@ -0,0 +1,59 @@
+// // Convert into a drawBox function
+
+// function drawBox(numRows, numCols, boxChar = "X") {
+//     for (let r = 0; r < 5; r++) {
+//         let line = "";
+//         for (let c = 0; c < 10; c++) {
+//             line += boxChar;
+//         }
+//         console.log(line);
+//     }
+ 
+// }
+// drawBox(3, 5, "!");
+
+
+
+// // Function name is omitted
+
+// let displaySum = function(x, y, z) {
+//     console.log(x + y + z);
+//  }
+//  // Function call
+//  displaySum(2, 5, 3);
+
+
+
+
+
+
+// let findSum = (a, b) => a + b;
+
+// let sum = findSum(3, 6);
+// console.log(sum);
+
+// let square = x => x * x;
+
+// console.log(square(5));
+
+
+
+// function displayConcat(param1, param2) {
+//     console.log(param1 + ", " + param2);
+// }
+//  // Code will be tested once with arguments "After", "noon" and again with arguments "Hair", "cut"
+//  displayConcat('After', 'noon');
+ 
+
+
+
+
+// let madLib = function(adjective, adverb, noun1, noun2, verb) {
+//     return "The " + adjective + noun1 + adverb + verb + " the " + noun2;
+// }
+// console.log(madLib("lazy", "quietly", "dog", "moon", "smashed"));
+
+
+
+
+ 
